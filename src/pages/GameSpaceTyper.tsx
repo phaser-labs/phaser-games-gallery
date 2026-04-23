c84ec68e08e19382561510e84d6d93473dad3e70
bb-frontend-11 <desarrolladorfrontend2@booksandbooksdigital.com.co>
2026-04-23 18:13:47 -0500

    fix: update game description and feature highlights in Space Typer page

--- src/pages/GameSpaceTyper.tsx
+++ src/pages/GameSpaceTyper.tsx
@@ -18,19 +18,19 @@ export const SpaceTyperPage = () => {
             <div className="u-flow m-4 mx-8">
               <h2 className=" u-fs-400">Descripción del juego:</h2>
               <p>
-
+                Space Typer es un juego de mecanografía ambientado en el espacio, donde los jugadores deben defender su nave espacial de oleadas de enemigos escribiendo palabras correctamente. El objetivo es mejorar la velocidad y precisión de mecanografía mientras se disfruta de una experiencia de juego emocionante y desafiante.
               </p>
               <p className="u-fs-300 u-font-bold">Características:</p>
               <ul className="u-flow list_star">
 
                 <li>
                   <p>
-                    <strong>Personalización:</strong>
+                    <strong>Personalización:</strong> Cantidad de palabras ilimitada, dando la posibilidad de utilizar cualquier tema o vocabulario que el jugador desee, desde palabras comunes hasta términos técnicos o de fantasía.
                   </p>
                 </li>
                 <li>
                   <p>
-                    <strong>Recomendación importante:</strong>
+                    <strong>Recomendación importante:</strong> Solo recibe palabras, no frases completas, para mantener la jugabilidad fluida y desafiante.
                   </p>
                 </li>
               </ul>
